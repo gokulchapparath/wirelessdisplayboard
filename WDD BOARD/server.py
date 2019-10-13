c3b4e3fc093a6c840a0bd34e80e90fb1c3bb3857
gokul chapparath <gokulchapparath@gmail.com>
2019-10-13 23:06:15 +0530

    oct 13 2019

---
+++ WDD BOARD/server.py
@@ -0,0 +1,29 @@
+from flask import Flask, render_template, url_for
+
+app= Flask(__name__, static_url_path='/static')
+
+@app.route("/")
+@app.route("/home")
+def defpage():
+    return render_template('home.html',  methods=['GET', 'POST'])    
+
+@app.route("/login")
+def login():
+    return render_template('login.html')
+
+@app.route("/registration")
+def registration():
+    return render_template('registration.html')
+
+@app.route("/regi")
+def regi():
+    return render_template('regi.html')
+
+@app.route("/add")
+def add():
+    return render_template('add.html')
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
